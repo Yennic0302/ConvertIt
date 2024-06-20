263a536e4dc671860b7f094f6618f0b357b5934a
Nicyen0302 <yender.yr45@gmail.com>
2024-06-20 03:28:25 -0400

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <nav className="flex justify-between items-center p-3 px-6  w-full">
+      <div className="">
+        <a className="text-2xl font-bold text-[--main-text]">ConverIt</a>
+      </div>
+    </nav>
+  );
+}
